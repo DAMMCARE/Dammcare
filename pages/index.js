ea33152a2ed879975408e355dd18eaf36103a5ce
DAMMCARE <gramettendaf@gmail.com>
2025-04-06 16:29:24 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welkom bij DAMM Care 💚</h1>
+      <p>Menselijke aandacht, elke dag opnieuw.</p>
+    </div>
+  );
+}
